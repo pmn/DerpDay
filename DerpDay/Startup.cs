c4927f842a3839edd8cf9e0d34beabced865abae
Paul Nakata <pmn@github.com>
2016-03-25 13:41:18 -0700

    Add all the things

---
+++ DerpDay/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DerpDay.Startup))]
+namespace DerpDay
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
